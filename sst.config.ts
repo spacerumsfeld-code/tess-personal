05fd838e662f7704f6370617d7870b39ce5c1261
Nick Papadakis <nickfin2014@gmail.com>
2026-02-26 20:52:39 -0700

    deploy action

---
+++ sst.config.ts
@@ -0,0 +1,29 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "tess-personal",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    const func = new sst.aws.Function("TessFunc", {
+      handler: "./",
+      runtime: 'go',
+      url: true,
+    });
+
+    new sst.aws.Router("TessRouter", {
+      domain: {
+        name: 'twflyfishing.com',
+        redirects: ['www.twflyfishing.com']
+      },
+      routes: {
+        "/*": func.url,
+      }
+    })
+  },
+});
