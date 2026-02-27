6940bfeb6dede0e85c3a9dacef02811cfa8203d7
Nick Papadakis <nickfin2014@gmail.com>
2026-02-26 22:11:42 -0700

    revise run func and use adapter for aws api gateway request eventss

--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/aws/aws-lambda-go v1.52.0
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/awslabs/aws-lambda-go-api-proxy v0.16.2 // indirect
--- main.go
+++ main.go
@@ -2,68 +2,24 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 
 	"tess-personal/internal/server"
 )
 
+var adapter *httpadapter.HandlerAdapterV2
+
+func init() {
+	adapter = httpadapter.NewV2(server.NewServer())
+}
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return adapter.ProxyWithContext(ctx, req)
+}
+
 func main() {
 	lambda.Start(handler)
-}
-
-func handler() {
-	if err := run(context.Background()); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
-	}
-}
-
-func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	srv := server.NewServer()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	httpServer := &http.Server{
-		Addr:         net.JoinHostPort("", port),
-		Handler:      srv,
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	go func() {
-		log.Printf("Starting server on port %s", httpServer.Addr)
-		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Printf("http server error: %s", err)
-		}
-	}()
-
-	<-ctx.Done()
-
-	log.Println("shutting down gracefully, press Ctrl+C again to force")
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := httpServer.Shutdown(shutdownCtx); err != nil {
-		return fmt.Errorf("server forced to shutdown: %w", err)
-	}
-
-	log.Println("Server exiting")
-	return nil
 }
